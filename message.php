2fdacc90dec695969f758f2bd42bf3ad83696be3
analou21 <anais_59430@hotmail.fr>
2016-12-02 07:46:00 +0100

    Ajout code pour la date

--- message.php
+++ message.php
@@ -1,27 +1,14 @@
 <?php
-include ('includes/connexion.inc.php');
+	include ('includes/connexion.inc.php');
 
-$id = $_POST['id'];
-$message = $_POST['message'];
-
-if (isset($message) && !empty($message)) {
-	if(isset($id) && !empty($id)){
-		$query = 'UPDATE messages SET contenu=:contenu WHERE id=:id';
+	if (isset($_POST['message']) && !empty($_POST['message']))
+	{
+		$query = 'INSERT INTO messages (contenu) VALUES (:contenu)';
 		$prep = $pdo->prepare($query);
-		$prep->bindValue(':contenu', $message);
-		$prep->bindValue(':id',$id);
-	$prep->execute();
-
-
-}else{
-	$query = 'INSERT INTO messages (contenu) VALUES (:contenu)';
-	$prep = $pdo->prepare($query);
-	$prep->bindValue(':contenu', $message);
-	$prep->execute();
-
-	}
-
-}
-header('Location: index.php');
-  exit();
+		$prep->bindValue(':contenu', $_POST['message']);
+		$prep->execute();
+	}else{
+ 	}
+	header('Location: index.php');
+ 		exit();
 ?>
